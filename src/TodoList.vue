55f7e2d23b25b7676415b65a73a7d519ac258a45
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-22 13:42:14 +0800

    feat:header

---
+++ src/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+  
+  <h2>Jquery To Do List</h2>
+  <p>Simple Todo List with adding and filter by diff status.</p>
+
+
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+
+
+
+
+
+
+}
+</style>
