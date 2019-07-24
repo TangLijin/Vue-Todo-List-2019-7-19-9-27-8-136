7b4cda2815f51a8bacf3d80ad064c759c0657636
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-24 08:42:36 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import App from './TodoList.vue'
 
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+    state: {
+        // count: 8,
+        // base: 2
+    },
+    getters: {
+        // getCountWithSomethingCool: function() {
+        //     return function() {
+        //         return store.state.count - 1;
+        //     }
+        // }
+    },
+    mutations: {
+        // increment(state) {
+        //     state.base += 10;
+        // }
+    }
+})
 
 new Vue({
     render: h => h(App),
